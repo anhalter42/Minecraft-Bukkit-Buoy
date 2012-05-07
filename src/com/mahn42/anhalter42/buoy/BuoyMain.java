9767bcb1731ba6f45c85bf802785b535decafd9f
Andre Schloemp <andre.schloemp@me.com>
2012-05-07 23:19:04 +0200

    netbeans

---
+++ src/com/mahn42/anhalter42/buoy/BuoyMain.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mahn42.anhalter42.buoy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author andre
+ */
+public class BuoyMain extends JavaPlugin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+
+    @Override
+    public void onEnable() { 
+        //this.getServer().getPluginManager().registerEvents(new VehicleListener(this), this);
+    }
+}
